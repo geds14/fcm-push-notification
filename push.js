8e88e725a0a2278b1ff54e118a280792c90daf0b
Gil <gsamonte@citronsolutions.com>
2020-08-18 22:11:13 +0800

    initital

---
+++ push.js
@@ -0,0 +1,6 @@
+var push =require('web-push');
+
+let vapidKeys = {
+    publicKey: 'BJaX-DgTzkLqyRVIIKzIObQ1fvlB3sLybqD64jqbA86-E8W3eXugQqJHqXOihe-5_LoFG-p684oVw4f17theJ1s',
+    privateKey: 'CqcqhxrIrXQNGtg0c_8301A8qIDxUYVn0yddumAhvDY'
+  }
